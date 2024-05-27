00e3bc3187a202a400df81ef66403baa8f8ef681
Santosh-Rashinkar <santoshrashinkar0108@gmail.com>
2024-05-27 17:13:36 +0530

    Day- 2 WAP to initialize char,int,double & float data type variables with any value

---
+++ src/com/prowings/coding/challenge/Day_2.java
@@ -0,0 +1,20 @@
+package com.prowings.coding.challenge;
+
+// WAP to initialize char,int ,double & float data type variables with any value .
+
+public class Day_2 {
+
+	public static void main(String[] args) {
+	
+		char x = 'a';
+		
+		int y = 10;
+		
+		double z= 56.453833;
+		
+		float w = 57.404556f;
+		
+		System.out.println("Variables are "+ x + y + z + w);
+
+	}
+}
