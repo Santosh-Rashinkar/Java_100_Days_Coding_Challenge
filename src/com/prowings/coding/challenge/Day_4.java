37c63ce2eb75c5e7beb82ad3f7cd0fe0e08ee905
Santosh-Rashinkar <santoshrashinkar0108@gmail.com>
2024-05-29 20:01:07 +0530

    WAP to find a largest and smallest number.,out of 3 "withaut using if...else" .

---
+++ src/com/prowings/coding/challenge/Day_4.java
@@ -0,0 +1,19 @@
+package com.prowings.coding.challenge;
+
+// WAP to find a largest and smallest number., out of 3 "withaut using if...else" .
+
+public class Day_4 {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		
+		int b = 7;
+		
+		int c = 9;
+		
+		int d = Math.min(a, Math.min(b,c));
+		
+		System.out.println("smallest number : " +d);
+	}
+}
