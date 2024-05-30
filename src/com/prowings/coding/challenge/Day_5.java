e406218b6c87ae919d552b1d9e8a43e45fd7fbf6
Santosh-Rashinkar <santoshrashinkar0108@gmail.com>
2024-05-30 13:53:46 +0530

    Day-5 WAP to print an Integer (Entered by the User )

---
+++ src/com/prowings/coding/challenge/Day_5.java
@@ -0,0 +1,17 @@
+package com.prowings.coding.challenge;
+
+import java.util.Scanner;
+
+// WAP to print an Integer (Entered by the User )
+
+public class Day_5 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int a = scan.nextInt();
+		System.out.println("Number is : " + a);
+		
+	}
+}
