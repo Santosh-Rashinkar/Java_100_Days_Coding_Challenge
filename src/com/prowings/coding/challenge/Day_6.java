c0475d57ae05d7cea996ec01ed940321ccac8eb8
Santosh-Rashinkar <santoshrashinkar0108@gmail.com>
2024-05-31 21:29:14 +0530

    Day-6 WAP Addition of two integer using scanner.

---
+++ src/com/prowings/coding/challenge/Day_6.java
@@ -0,0 +1,25 @@
+package com.prowings.coding.challenge;
+
+import java.util.Scanner;
+
+// WAP Addition of two integer using scanner .
+
+public class Day_6 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter a two number : ");
+
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = a + b;
+
+		System.out.println("Addition of two integer :" + c);
+
+		scan.close();
+
+	}
+
+}
