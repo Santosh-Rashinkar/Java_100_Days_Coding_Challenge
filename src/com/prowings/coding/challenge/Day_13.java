c5a086df9ac51eaeaf2d65b209c9255ed15a9188
Santosh-Rashinkar <santoshrashinkar0108@gmail.com>
2024-06-15 19:50:27 +0530

    Day-13 WAP to Display Fibonacci Series

--- src/com/prowings/coding/challenge/Day_13.java
+++ src/com/prowings/coding/challenge/Day_13.java
@@ -6,13 +6,14 @@ public class Day_13 {
 
 	public static void main(String[] args) {
 		int n = 10, ft = 0, st = 1;
-		System.out.println("fibonacci series till " + n + "terms :");
+		System.out.println("Fibonacci Series till " + n + "terms :");
 
 		for (int i = 1; i <= n; i++) {
 			System.out.println(ft + " ");
 			int nt = ft + st;
 			ft = st;
 			st = nt;
+			
 		}
 	}
 }
