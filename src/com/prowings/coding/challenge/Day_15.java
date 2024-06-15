4319099d2d47684d77f44a1efbe818e389dccf55
Santosh-Rashinkar <santoshrashinkar0108@gmail.com>
2024-06-15 19:46:54 +0530

    Day- 15 WAP to Calculates the LCM of the two entered integers Prints the LCM(Least Common Multiple)

---
+++ src/com/prowings/coding/challenge/Day_15.java
@@ -0,0 +1,25 @@
+package com.prowings.coding.challenge;
+
+import java.util.Scanner;
+
+public class Day_15 {
+
+	public static void main(String[] args) {
+		
+		int lcm;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number :");
+		int n1 = scan.nextInt();
+		int n2 = scan.nextInt();
+		
+		lcm =(n1 > n2) ? n1 : n2;
+		
+		while(true) {
+			if(lcm % n1 == 0 && lcm % n2 == 0) {
+				System.out.printf("The LCM of %d and %d is %d.",n1,n2,lcm);
+				break;
+			}
+			lcm++;
+		}
+	}
+}
