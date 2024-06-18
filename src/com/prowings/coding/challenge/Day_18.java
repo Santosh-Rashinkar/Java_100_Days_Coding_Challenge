0ede1e4952a6a00ab1e6aa1228a0b41ace56ab8b
Santosh-Rashinkar <santoshrashinkar0108@gmail.com>
2024-06-18 14:42:40 +0530

    Day-18 WAP to check whether a number is Prime or Not

--- src/com/prowings/coding/challenge/Day_18.java
+++ src/com/prowings/coding/challenge/Day_18.java
@@ -6,27 +6,27 @@ import java.util.Scanner;
 public class Day_18 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter a number: ");
 		int number = scan.nextInt();
-		
+
 		isPrimeSimple(number);
-		
+
 		scan.close();
 	}
-	
+
 	public static void isPrimeSimple(int number) {
-        if (number <= 1) {
-            System.out.println(number + " is not a prime number.");
-            return;
-        }
-        for (int i = 2; i < number; i++) {
-            if (number % i == 0) {
-                System.out.println(number + " is not a prime number.");
-                return;
-            }
-        }
-        System.out.println(number + " is a prime number.");
-    }
+		if (number <= 1) {
+			System.out.println(number + " is not a prime number.");
+			return;
+		}
+		for (int i = 2; i < number; i++) {
+			if (number % i == 0) {
+				System.out.println(number + " is not a prime number.");
+				return;
+			}
+		}
+		System.out.println(number + " is a prime number.");
+	}
 }
