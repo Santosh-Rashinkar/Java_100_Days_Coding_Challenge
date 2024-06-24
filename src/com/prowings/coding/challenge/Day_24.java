f8fe9224cf4fbdb261e17bfce6e46f65e78c2a25
Santosh-Rashinkar <santoshrashinkar0108@gmail.com>
2024-06-25 00:21:58 +0530

    Day-24 WAP to Calculate Difference Between Two Time Periods

--- src/com/prowings/coding/challenge/Day_24.java
+++ src/com/prowings/coding/challenge/Day_24.java
@@ -1,30 +1,40 @@
 package com.prowings.coding.challenge;
 
-import java.sql.Date;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 //WAP to Calculate Difference Between Two Time Periods .
 
 public class Day_24 {
 
 	public static void main(String[] args) {
-		
+
 		String time1 = "18:00:00";
 		String time2 = "7:30:50";
+
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+
+		try {
+ 
+		Date date1 = simpleDateFormat.parse(time1);
+		 
+		Date date2 = simpleDateFormat.parse(time2);
+
+		long differenceInMilliSeconds = Math.abs(date2.getTime() - date1.getTime());
+
+		long differenceInHours = (differenceInMilliSeconds / (60 * 60 * 1000)) % 24;
 		
-		SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+		long differenceInMinutes = (differenceInMilliSeconds / (60 * 1000)) % 60;
 		
-		Date date1 = dateFormat.parse(time1);
-		Date date2 = dateFormat.parse(time2);
-		
-		long differenceInMilliSeconds = Math.abs(date2.getTime()-date1.getTime());
-		
-		long differenceInHours = (differenceInMilliSeconds/(60*60*1000))%24;
-		long differenceInMinutes = (differenceInMilliSeconds/ (60*1000))%60;
-		long differenceInSeconds = (differenceInMilliSeconds/1000)%60;
-		
-		System.out.println("Difference is "+differenceInHours+"hours"+differenceInMinutes+"mi");
-		
+		long differenceInSeconds = (differenceInMilliSeconds / 1000) % 60;
+
+		System.out.println("Difference is " + differenceInHours + " hours " + differenceInMinutes + " minutes "
+				+ differenceInSeconds + " Seconds. ");
 	}
-	
+	catch (ParseException e) {	
+		e.printStackTrace();
+	}
+  }
+
 }
